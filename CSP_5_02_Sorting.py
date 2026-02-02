308ab821ec1844973106a9bca5e8f0ef7b772c3b
lmills26 <lmills26@bentleyschool.org>
2026-02-02 10:28:31 -0800

    Add files via upload

--- CSP_5_02_Sorting.py
+++ CSP_5_02_Sorting.py
@@ -4,19 +4,48 @@ import random
 def bubbleSort(items:list):
     swaps = 0
     comparisons = 0
-
+    while True:
+        swapsInLoop = 0
+        for i in range(0, len(items)-1):
+            comparisons += 1
+            if items[i] > items[i+1]:
+                items[i], items[i+1] = items[i+1], items[i]
+                swaps +=1
+                swapsInLoop +=1
+        if swapsInLoop == 0:
+            break
     return items, swaps, comparisons
 
 def insertionSort(items: list):
     swaps = 0
     comparisons = 0
+    for i in range(1, len(items)):
+        key = items[i]
+        j = i - 1
 
+        while j >= 0:
+            comparisons += 1
+            if items[j] > key:
+                items[j + 1] = items[j]
+                swaps += 1
+                j -= 1
+            else:
+                break
+        items[j + 1] = key
     return items, swaps, comparisons
 
 def selectionSort(items : list):
     swaps = 0
     comparisons = 0
+    for i in range(0, len(items)-1):
+        nextSmallestpos = i
+        for j in range(i+1, len(items)):
+            comparisons +=1
+            if items[j] < items[nextSmallestpos]:
+                nextSmallestpos = j
 
+        items[i], items[nextSmallestpos] = items[nextSmallestpos], items[i]
+        swaps += 1
     return items, swaps, comparisons
 
 
